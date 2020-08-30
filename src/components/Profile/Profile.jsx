87d3f9a19dc303424cc26c13440248e48451cbb8
OleksandrLevak <sashavytvyckyj@gmail.com>
2020-08-30 21:36:48 +0300

    Folders of component, MyPosts, Post, css-modules

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import s from "./Profile.module.css";
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () => {
+  return (
+    <div className={s.content}>
+      <div>
+        <img src="https://colorlib.com/wp/wp-content/uploads/sites/2/react-dev-tools-logo.jpg" />
+      </div>
+      <div>
+        <div>Hi, I am Oleksandr Levak</div>
+      </div>
+      <MyPosts />
+    </div>
+  );
+};
+
+export default Profile;
