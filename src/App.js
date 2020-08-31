cf05f9187f5b23030ebe8e0b47d5332af53c4f61
OleksandrLevak <sashavytvyckyj@gmail.com>
2020-08-31 10:44:30 +0300

    Dialogs, update App.css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Profile from "./components/Profile/Profile.jsx";
 import Nav from "./components/Nav/Nav.jsx";
+import Dialogs from "./components/Dialogs/Dialogs";
 const App = () => {
   return (
     <div className="wrapper">
       <Header />
       <Nav />
-      <Profile />
+      <div className="wrapper-content">
+        <Profile />
+      </div>
     </div>
   );
 };
