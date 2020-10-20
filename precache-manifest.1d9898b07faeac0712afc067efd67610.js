0ced30067acef018627231e418dfe4d8c5c98126
Oleksandr Levak <sashavytvyckyj@gmail.com>
2020-10-20 20:07:16 +0300

    Updates

---
+++ precache-manifest.1d9898b07faeac0712afc067efd67610.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc00db4c789328bb295fcbb300f0dfa",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "69ec52027597e291b109",
+    "url": "/SocialNetwork/static/css/3.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "839194036aed21939f1f",
+    "url": "/SocialNetwork/static/css/4.9f2edd56.chunk.css"
+  },
+  {
+    "revision": "2ca1006f8dc4d6c9d8c4",
+    "url": "/SocialNetwork/static/css/5.49508f90.chunk.css"
+  },
+  {
+    "revision": "cc56fe2fc98bf64576cc",
+    "url": "/SocialNetwork/static/css/6.1345a68a.chunk.css"
+  },
+  {
+    "revision": "86e5b69c4887af78ff9a",
+    "url": "/SocialNetwork/static/css/7.7a31b8c5.chunk.css"
+  },
+  {
+    "revision": "19ae7f565417f2d5d59e",
+    "url": "/SocialNetwork/static/css/main.c63a7f96.chunk.css"
+  },
+  {
+    "revision": "a5b266f0e3a30d2d7b4d",
+    "url": "/SocialNetwork/static/js/0.2f5c80d6.chunk.js"
+  },
+  {
+    "revision": "69ec52027597e291b109",
+    "url": "/SocialNetwork/static/js/3.cec8ee28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork/static/js/3.cec8ee28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "839194036aed21939f1f",
+    "url": "/SocialNetwork/static/js/4.32a4d6e3.chunk.js"
+  },
+  {
+    "revision": "2ca1006f8dc4d6c9d8c4",
+    "url": "/SocialNetwork/static/js/5.f33666f0.chunk.js"
+  },
+  {
+    "revision": "cc56fe2fc98bf64576cc",
+    "url": "/SocialNetwork/static/js/6.16dcd499.chunk.js"
+  },
+  {
+    "revision": "86e5b69c4887af78ff9a",
+    "url": "/SocialNetwork/static/js/7.f4771ada.chunk.js"
+  },
+  {
+    "revision": "19ae7f565417f2d5d59e",
+    "url": "/SocialNetwork/static/js/main.06f24dcc.chunk.js"
+  },
+  {
+    "revision": "4cbdb30a140283a32d4e",
+    "url": "/SocialNetwork/static/js/runtime-main.90e88701.js"
+  },
+  {
+    "revision": "30bbdd8ef62ac2320a1bfe4917663ad9",
+    "url": "/SocialNetwork/static/media/preloader.30bbdd8e.svg"
+  },
+  {
+    "revision": "23a5577ba03150f579e2f2932d61c827",
+    "url": "/SocialNetwork/static/media/user.23a5577b.png"
+  }
+]);
